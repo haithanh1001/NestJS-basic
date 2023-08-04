1a89f32cd6958e9df12fe4bb4e1e413d79921699
haithanh <haithanh9718@gmail.com>
2023-08-04 18:37:41 +0700

    connect database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  app.useStaticAssets(join(__dirname, '..', 'public')); //js css image
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // stored html
+  app.setViewEngine('ejs'); //set view engine
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
