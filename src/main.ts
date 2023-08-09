a765748a733b7ebb040b98caf48e5299ae1679a6
haithanh <haithanh9718@gmail.com>
2023-08-09 23:03:07 +0700

    disable guard

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
@@ -8,6 +9,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useStaticAssets(join(__dirname, '..', 'public')); //js css image
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // stored html
   app.setViewEngine('ejs'); //set view engine
