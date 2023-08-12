f302750064a8c45970bbfced486a835e0d3291bb
haithanh <haithanh9718@gmail.com>
2023-08-12 11:15:08 +0700

    update user model + create-user-dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,60 @@
+import mongoose from 'mongoose';
+
 //DTO
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Prop, Schema } from '@nestjs/mongoose';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
+  name: string;
+}
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name khong duoc de trong' })
+  name: string;
   @IsEmail({}, { message: 'Email phai dung dinh dang' })
   @IsNotEmpty({ message: 'Email khong duoc de trong' })
   public email: string; //default: public
   @IsNotEmpty({ message: 'Password khong duoc de trong' })
   password: string;
+  @IsNotEmpty({ message: 'Age khong duoc de trong' })
+  age: number;
+  @IsNotEmpty({ message: 'Gender khong duoc de trong' })
+  gender: string;
+  @IsNotEmpty({ message: 'Address khong duoc de trong' })
+  address: string;
+  @IsNotEmpty({ message: 'Role khong duoc de trong' })
+  @Prop()
+  role: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Name khong duoc de trong' })
   name: string;
+  @IsEmail({}, { message: 'Email phai dung dinh dang' })
+  @IsNotEmpty({ message: 'Email khong duoc de trong' })
+  public email: string; //default: public
+  @IsNotEmpty({ message: 'Password khong duoc de trong' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Age khong duoc de trong' })
+  age: number;
+  @IsNotEmpty({ message: 'Gender khong duoc de trong' })
+  gender: string;
+  @IsNotEmpty({ message: 'Address khong duoc de trong' })
   address: string;
 }
